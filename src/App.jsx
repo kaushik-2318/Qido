6837347533495655f539fa8833497ec8ee7313cd
kaushik-2318 <kaushikverma321@gmail.com>
2024-08-24 01:03:30 +0530

    Loading Animation Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import React, { useState } from 'react';
+import Loading from './components/Loader'
 
 function App() {
 
-  const formats = ['png', 'base64'];
+  const formats = ['png', 'svg'];
   const sizes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
   const [qr, setQr] = useState("")
+  const [loading, setLoading] = useState("false");
   const [isActive, setActive] = useState(false);
   const [text, setText] = useState("");
   const [format, setFormat] = useState('png');
@@ -21,7 +23,7 @@ function App() {
   const [caption, setCaption] = useState("");
   const [fontfamily, setFontFamily] = useState('sans-serif');
   const [fontsize, setFontSize] = useState(10);
-  const [down, setDown] = useState("")
+  const [qrBlob, setQrBlob] = useState(null);
 
 
   const qrcode = async () => {
@@ -32,8 +34,13 @@ function App() {
     else {
       response = await fetch(`https://quickchart.io/qr?text=${text}&dark=${color}&light=${backgroundcolor}&ecLevel=Q&format=${format}&size=${sizeValue}&centerImageSizeRatio=${imageRatio}&centerImageUrl=${image}&caption=${caption}&captionFontFamily=${fontfamily}&captionFontSize=${fontsize}`);
     }
+
+    const blob = await response.blob();
+
+    setQrBlob(blob);
     setQr(response.url);
     setDown(response);
+    setLoading(false);
   };
 
   const ToggleClass = () => {
@@ -57,8 +64,15 @@ function App() {
 
     qrcode();
     ToggleClass();
+    setLoading(true);
   }
 
+  function calculateBytes(inputString) {
+    const encoder = new TextEncoder();
+    const encodedString = encoder.encode(inputString);
+    return encodedString.length;
+  }
+  
   const handleFormatChange = (option) => {
     setFormat(option.value);
   }
@@ -69,55 +83,59 @@ function App() {
 
   const handleDownload = async () => {
     try {
-      const response = down;
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
+      if (!qrBlob) {
+        alert("No QR Code to download.");
+        return;
+      }
+      const url = window.URL.createObjectURL(qrBlob);
       const link = document.createElement('a');
       link.href = url;
-      link.download = "QRCode.png";
+      link.download = `QRCode.${format}`;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
       window.URL.revokeObjectURL(url);
     } catch (error) {
       alert("Error in Downloading the QR Code");
-      console.log(error);
+      console.error("Download error:", error);
     }
   };
 
+
   const handleShare = async () => {
     try {
-      const response = down;
-      const blob = await response.blob();
-      const clipboardItem = new ClipboardItem({ 'image/png': blob });
+      if (!qrBlob) {
+        alert("No QR Code to share.");
+        return;
+      }
+      const clipboardItem = new ClipboardItem({ [format === 'svg' ? 'image/svg+xml' : 'image/png']: qrBlob });
       await navigator.clipboard.write([clipboardItem]);
       alert("QR Code image copied to clipboard!");
     } catch (error) {
-      alert("Error copying image to clipboard");
-      console.log(error);
+      console.error("Error copying image to clipboard:", error);
+      alert("Error copying image to clipboard. Please try again.");
     }
   };
+
 
   const handleBack = () => {
     ToggleClass();
   }
 
-  function calculateBytes(inputString) {
-    const encoder = new TextEncoder();
-    const encodedString = encoder.encode(inputString);
-    return encodedString.length;
-  }
-
   return (
     <>
-      <div className='bg-[#111729] background flex justify-center items-center duration-1000 flex-col gap-10 font-mono text-sm' style={isActive ? { height: "100vh" } : { height: caption != "" ? "130vh" : "120vh" }}>
-        <div className="text-blue-700 font-['Roboto'] uppercase text-4xl">QRCode Generator</div>
+      <div className='bg-[#111729] background flex justify-center items-center flex-col gap-10 font-mono text-sm pt-10 sm:py-10' style={{ height: isActive ? "100vh" : "100%" }}>
+
+        <div className="text-blue-700 font-['Roboto'] uppercase text-4xl py-5 sm:p-0">QRCode Generator</div>
+
         <div className='flex justify-center items-center w-full '>
 
           <div className='flex-col justify-center items-center gap-10' style={{ display: isActive ? "flex" : "none" }}>
             <div className='relative z-20 flex justify-center items-center'>
               <div className='bg-[#1e2c51] rounded-full absolute w-[280px] h-[280px] -z-10'></div>
-              <img className='z-10 rounded-xl w-[240px] h-[240px]' src={qr} alt="QR Code" />
+              {
+                loading ? <div className='z-10 rounded-xl w-[240px] h-[240px]'><Loading /></div> : <div><img className='z-10 rounded-xl w-[240px] h-[240px]' src={qr} alt="QR Code" /></div>
+              }
             </div>
             <div className='text-white'>
               (When you download or share it, you will get {sizeValue} X {sizeValue} QR Code.)
@@ -131,54 +149,54 @@ function App() {
             </div>
           </div>
 
-          <form action="" onSubmit={handleSubmit} className='flex flex-col justify-between text-[#a5afc2] font-medium border-2 border-[#3662e3] bg-[#030617] p-10 rounded-3xl duration-1000' style={{ height: caption != "" ? "750px" : "670px", display: isActive ? "none" : "flex" }}>
+          <form action="" onSubmit={handleSubmit} className={`flex flex-col justify-between text-[#a5afc2] font-medium border-t-4 sm:border-2 border-[#3662e3] bg-[#030617] p-5 sm:p-10 rounded-[30px] rounded-b-none sm:rounded-b-[30px] w-full sm:w-max duration-500  ${caption !== "" && image !== "" ? "h-[770px]" : caption !== "" ? "h-[690px]" : image !== "" ? "h-[650px] sm:h-[680px]" : "h-[630px] sm:h-[600px]"}`} style={{ display: isActive ? "none" : "flex" }}>
             <div className='flex flex-col gap-10 h-max'>
+
               <label className='flex justify-between items-center w-full'>
                 <div>Text: <span className='text-red-500'>*</span></div>
                 <input onInput={e => setText(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" name="text" required />
               </label>
+
               <label className='flex justify-between items-center gap-10 w-full'>
                 <div>Format:</div>
                 <Dropdown options={formats.map(format => ({ value: format, label: format }))} onChange={handleFormatChange} placeholder="Select an option" className='w-48 font-normal' />
               </label>
-              <div className='flex flex-row justify-between items-center gap-10'>
-                <label className='flex flex-col justify-center items-start gap-3'>
+
+              <div className='flex flex-col sm:flex-row justify-between items-center gap-10'>
+                <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3'>
                   <div>Size:</div>
                   <input onInput={e => setSizeValue(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="number" placeholder='150px' />
                 </label>
                 {caption == "" && image == "" && (
-                  <label className='flex flex-col justify-center items-start gap-3 duration-700 transition ease-in-out opacity-100 translate-y-0' >
+                  <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3' >
                     <div>Margin (in px):</div>
                     <input onInput={e => setMargin(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="number" placeholder='4px' />
                   </label>
                 )}
               </div>
 
-              <div className='flex flex-row justify-between items-center gap-10'>
-                <label className='flex flex-col justify-center items-start gap-3'>
+              <div className='flex flex-col sm:flex-row justify-between items-center gap-10'>
+                <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3'>
                   <div>Background Color:</div>
-                  <input onInput={e => setBackgroundColor(e.target.value.replace(/^#/, ''))} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='FFFFFF' />
+                  <input onInput={e => setBackgroundColor(e.target.value.replace(/^#/, ''))} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='000000' />
                 </label>
-                <label className='flex flex-col justify-center items-start gap-3'>
+                <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3'>
                   <div>QR Code Color:</div>
-                  <input onInput={e => setColor(e.target.value.replace(/^#/, ''))} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='000000' />
+                  <input onInput={e => setColor(e.target.value.replace(/^#/, ''))} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='FFFFFF' />
                 </label>
               </div>
 
-              <div className='flex flex-col gap-6'>
+              <div className='flex flex-col gap-10'>
                 <label className='flex  justify-between items-center gap-10 w-full'>
                   <div>Image in QR:</div>
                   <input onInput={e => setImage(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='URL' />
                 </label>
-
-                <div className='flex flex-col gap-4'>
-                  {image != "" && (
-                    <label className='flex justify-between items-center gap-10 w-full'>
-                      <div>Image Size Ratio</div>
-                      <Dropdown options={sizes.map(size => ({ value: size, label: size }))} onChange={handleImageRatioChange} placeholder="Select an option" className='w-48 font-normal' />
-                    </label>
-                  )}
-                </div>
+                {image != "" && (
+                  <label className='flex justify-between items-center gap-10 w-full'>
+                    <div>Image Size Ratio</div>
+                    <Dropdown options={sizes.map(size => ({ value: size, label: size }))} onChange={handleImageRatioChange} placeholder="Select an option" className='w-48 font-normal' />
+                  </label>
+                )}
               </div>
 
               <div className='flex flex-col gap-6'>
@@ -186,26 +204,27 @@ function App() {
                   <div>Caption:</div>
                   <input onInput={e => setCaption(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" />
                 </label>
-
                 {caption != "" && (
-                  <div className='flex gap-10 duration-200'>
-                    <label className='flex flex-col justify-center items-start gap-3'>
+                  <div className='flex flex-col sm:flex-row gap-10'>
+                    <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3'>
                       <div>Font Family:</div>
-                      <input onInput={e => setFontFamily(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="number" placeholder='Sans-Serif' />
+                      <input onInput={e => setFontFamily(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="text" placeholder='Sans-Serif' />
                     </label>
-                    <label className='flex flex-col justify-center items-start gap-3'>
+                    <label className='flex flex-row sm:flex-col justify-between sm:justify-center items-center w-full sm:items-start gap-3'>
                       <div>Font Size:</div>
                       <input onInput={e => setFontSize(e.target.value)} className='font-normal p-1 px-3 text-black outline-none rounded-sm w-48' type="number" placeholder='10px' />
                     </label>
                   </div>
                 )}
               </div>
+
             </div>
 
             <div className='flex justify-around items-center flex-row gap-20 text-white font-medium'>
               <button type="submit" className='bg-[#3662e3] px-10 py-3 rounded-md'>Submit</button>
               <button type="reset" className='bg-[#3662e3] px-10 py-3 rounded-md'>Reset</button>
             </div>
+
           </form>
         </div>
       </div>
